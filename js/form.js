3581d0d48592dbacfc943f71f6275d7f1bfb9f30
Jason Hanes <jhanes@hobiecat.com>
2017-04-18 17:04:12 -0700

    Lots o' changes

--- js/form.js
+++ js/form.js
@@ -6,19 +6,39 @@ $(document).ready(function() {
         // Stop form from actually submitting on button click
         event.preventDefault();
 
+        // Setup dummy/test data in localStorage
+        if ($('input.input-email').val() == 'SPAM_LOCALSTORAGE') {
+            for (var i = 250; i < 500; i++) {
+                // Fake data vars
+                var email = i + '@hobie.com';
+                var zip = '98765';
+
+                // Create JSON object to pass along to localStorage
+                var json_data = {'email_address': email, 'postal_code': zip, 'source_id': '12', 'mailpiece_slug': 'eclipse-email-newsletter'};
+
+                // Get number of items stored in localStorage
+                var num_rows = localStorage.length + 1;
+
+                // Write JSON data to localStorage
+                localStorage.setItem(num_rows, JSON.stringify(json_data));
+            }
+        }
+
         // Clear all localStorage data
-            if ($('input.input-email').val() == 'DELETE_ALL_DATA') {
+        if ($('input.input-email').val() == 'DELETE_ALL_DATA') {
             // Clear all localStorage data
             localStorage.clear();
 
             // Show message when localStorage data has been cleared
-            $('div.validation-messages').append('<label class="form-success">All stored data has been deleted!</label>').delay(3000).fadeOut(500, function() {
-                location.reload();
-            });
+            $('div.validation-messages').append('<label class="form-success">All stored data has been deleted!</label>');
         }
 
         // Loop through and show all localStorage data
         else if ($('input.input-email').val() == 'SHOW_ALL_DATA') {
+            // Show number of displayed rows
+            $('div.validation-messages').append('<label class="form-success">Displaying ' + localStorage.length + ' leads.</label>')
+
+            // Loop through and show all stored data
             for (var i = 0; i < localStorage.length; i++) {
                 var obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
                 $('div.validation-messages').append('<label class="form-success">EMAIL: ' + obj.email_address + ' ZIP: ' + obj.postal_code + '</label>');
@@ -27,17 +47,22 @@ $(document).ready(function() {
 
         // Upload stored data via AJAX
         else if ($('input.input-email').val() == 'UPLOAD_ALL_DATA') {
+            // Show number of leads being uploaded
+            $('div.validation-messages').append('<label class="form-success">Uploading ' + localStorage.length + ' leads.</label>')
+
+            // Loop through and upload all stored data
             for (var i = 0; i < localStorage.length; i++) {
                 var obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
-                $.post('http://api.hobiecat-dev.com/lead-generation-tablet-post/', obj, function(data) {
-                    console.log(obj.email_address + ' ' + obj.postal_code);
-                }, 'json');
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://api.hobiecat-dev.com/lead-generation-tablet-post/',
+                    data: obj,
+                    async: false
+                });
             }
 
             // Show message when localStorage data has been uploaded
-            $('div.validation-messages').append('<label class="form-success">All stored data has been uploaded.</label>').delay(3000).fadeOut(500, function() {
-                location.reload();
-            });
+            $('div.validation-messages').append('<label class="form-success">All stored data has been uploaded.</label>');
         }
     });
 
